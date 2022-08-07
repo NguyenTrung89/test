3370ab5be3e99740fafa94b65805fbe06b5f3c47
NguyenTrung89 <trungnguyen.hust@gmail.com>
2022-08-07 17:37:01 +0700

    Test Tieng Viet

---
+++ 01_swapping_value_with_pointer.c
@@ -0,0 +1,2 @@
+/*Bai 01. Sử dụng con trỏ để gán giá trị cho 2 biến nguyên a, b. Tráo đổi giá trị của 2 biến này thông qua con trỏ, 
+hiển thị giá trị của 2 biến sau khi tráo đổi giá trị và địa chỉ vùng nhớ của 2 biến này.*/
